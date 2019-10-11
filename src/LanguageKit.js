10523a738ef256ded961d434d94373634491ac7f
Zanetti <znti.rafael@gmail.com>
2019-10-11 18:32:01 -0300

    Add chooseStrings functionality to manage string resources

--- src/LanguageKit.js
+++ src/LanguageKit.js
@@ -1,26 +1,45 @@
 import React, { createContext, useContext, useReducer } from 'react';
 
-const availableLanguages = [
-	'en',
-	'pt',
-]
+const LanguageContext = createContext();
 
-const defaultLanguage = availableLanguages[0];
-const LanguageContext = createContext(defaultLanguage);
-
-const reducer = (_, lang) => {
-	const languageIndex = availableLanguages.indexOf(lang);
-	if(languageIndex >= 0) {
-		return availableLanguages[languageIndex];
+const reducer = (state, language) => {
+	console.log('State', state, 'and lang', language);
+	if(state.options.includes(language)) {
+		return { ...state, language };
 	}
 };
 
-export default function LanguageProvider({children}) {
+export default function LanguageProvider({children, languages, language}) {
+	
+	const options = languages || [ 'en', 'pt' ];
+	language = language || 'en';
+
 	return (
-		<LanguageContext.Provider value={useReducer(reducer, defaultLanguage)}>
+		<LanguageContext.Provider value={useReducer(reducer, {language, options})}>
 			{children}
 		</LanguageContext.Provider>
 	);
 }
 
 export const useLanguage = () => useContext(LanguageContext);
+
+export const chooseStrings = (options) => {
+	console.log('L.currentValue', LanguageContext._currentValue);
+	console.log('Choosing for lang', LanguageContext, 'from optinos', options);
+
+	const [ {language} ] = LanguageContext._currentValue;
+
+	const chosenStrings = options[language];
+
+	if(chosenStrings) {
+		return chosenStrings;
+	}
+
+	// Choose the first available option as default
+	if(Object.keys(options).length > 0) {
+		return options[Object.keys(options)[0]];
+	}
+
+	return {}
+}
+
